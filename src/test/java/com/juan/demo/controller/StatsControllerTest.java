f132aa2e6a9b0d4fde1abdfa95ade1e08b277999
Juan Hernandez <juanhernandez@paypal.com>
2018-02-23 03:18:21 -0600

    Adding another unit test to test when controller returns 500.

--- src/test/java/com/juan/demo/controller/StatsControllerTest.java
+++ src/test/java/com/juan/demo/controller/StatsControllerTest.java
@@ -16,6 +16,7 @@ import java.util.List;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -23,7 +24,12 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class StatsControllerTest {
 
-    private static final List<Double> TRANSACTION_VALUES = new ArrayList<>();
+    private static final List<Double> TRANSACTION_VALUES = new ArrayList<Double>() {{
+        add(10.50);
+        add(24.50);
+        add(65.00);
+        add(20.00);
+    }};
 
     @Mock
     private StatisticCalculatorService statisticCalculatorService;
@@ -36,10 +42,6 @@ public class StatsControllerTest {
     @Before
     public void setUp() {
         statsController = new StatsController(transacFeignClient, statisticCalculatorService);
-        TRANSACTION_VALUES.add(10.50);
-        TRANSACTION_VALUES.add(24.50);
-        TRANSACTION_VALUES.add(65.00);
-        TRANSACTION_VALUES.add(20.00);
     }
 
     @Test
@@ -59,5 +61,14 @@ public class StatsControllerTest {
         final Calcs returnedCalcs = (Calcs) responseEntity.getBody();
         assertThat(returnedCalcs.getAverage(), is(calcsMade.getAverage()));
         assertThat(returnedCalcs.getSum(), is(TRANSACTION_VALUES.stream().mapToDouble(f -> f.doubleValue()).sum()));
+    }
+
+    @Test
+    public void testGetStatisticsWhenWeGetException() {
+        when(transacFeignClient.getTransactions()).thenThrow(new RuntimeException());
+        final ResponseEntity responseEntity = statsController.getStatistics();
+        verify(transacFeignClient, times(1)).getTransactions();
+        verify(statisticCalculatorService, never()).getTransacsValues(any(List.class));
+        assertThat(responseEntity.getStatusCode().value(), is(500));
     }
 }
